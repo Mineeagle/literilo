a8daeacf916d2e55d845e4d15e550677d6921816
Mineeagle <streber123@streber24.de>
2025-06-09 21:18:26 +0200

    Ege etaj plibonigaĵoj

--- literilo.py
+++ literilo.py
@@ -18,26 +18,26 @@ MAPAĴOJ = {
 KLAVAR_KONTROLILO = keyboard.Controller()
 
 # Helpanta[j] variablo[j]
-tajpitaj_leteroj: str = ""
+tajpitaj_literoj: str = ""
 
 
 def _ricevu_mapaĵo() -> str | None:
     """
-    Ĉi tiu metodo simple revenigas mapadon, bazitan sur la lastaj tajpitaj literoj en tajpitaj_leteroj. Ekz.:
+    Ĉi tiu metodo simple revenigas mapadon, bazitan sur la lastaj tajpitaj literoj en tajpitaj_literoj. Ekz.:
     "suficx" -> "ĉ"
     "teksto" -> None
     "Hx"     -> "Ĥ"
     La rezulto baziĝas sur la mapado trovita en la dikto MAPAĴOJ supre.
     """
-    global tajpitaj_leteroj, MAPAĴOJ
+    global tajpitaj_literoj, MAPAĴOJ
 
     # Kontrolu ĉu ni havas almenaŭ du aliajn literojn skribitajn antaŭe
-    if len(tajpitaj_leteroj) < 1:
+    if len(tajpitaj_literoj) < 1:
         return None
 
     # Akiru la mapaĵon
     for key in MAPAĴOJ:
-        if tajpitaj_leteroj.endswith(key):
+        if tajpitaj_literoj.endswith(key):
             return MAPAĴOJ.get(key, None)
 
     # Se nenio valida estis trovita, revenigu None
@@ -49,11 +49,11 @@ def on_press(key) -> None:
     Ĉi tiu metodo estas vokita tuj kiam la uzanto premas klavon.
     Ĝi faras la jenajn aferojn:
     1. Ricevas uzantajn enigojn
-    2. Aktualigas la tajpitaj_leteroj literaron, donita nova vorto, ĝi estos purigita
+    2. Aktualigas la tajpitaj_literoj literaron, donita nova vorto, ĝi estos purigita
     3. Akiras eblan Esperantan literon
     4. Skribas la Esperantan literon
     """
-    global tajpitaj_leteroj, KLAVAR_KONTROLILO
+    global tajpitaj_literoj, KLAVAR_KONTROLILO
     # Traktas uzantajn enigojn
     try:
         litero = key.char
@@ -62,14 +62,15 @@ def on_press(key) -> None:
             return
         litero = " "
 
-    # Aktualigu tajpitaj_leteroj
+    # Aktualigu tajpitaj_literoj
     if litero == " ":
         ## Purigu la registritajn klavojn kaj revenu, tuj kiam ni havas novan vorton
-        tajpitaj_leteroj = ""
+        tajpitaj_literoj = ""
         return
     ## Aldonu al la listo de tajpitaj literoj
-    tajpitaj_leteroj += litero
-    print(tajpitaj_leteroj)
+    if tajpitaj_literoj == None:
+        return
+    tajpitaj_literoj += litero
 
     # Akiru eblan Esperantan karakteron por skribi
     esperanta_teksto = _ricevu_mapaĵo()
@@ -77,7 +78,6 @@ def on_press(key) -> None:
         return
 
     # Skribu Esperantan literon
-    KLAVAR_KONTROLILO.tap(keyboard.Key.backspace)
     KLAVAR_KONTROLILO.tap(keyboard.Key.backspace)
     KLAVAR_KONTROLILO.tap(keyboard.Key.backspace)
     KLAVAR_KONTROLILO.tap(esperanta_teksto)
